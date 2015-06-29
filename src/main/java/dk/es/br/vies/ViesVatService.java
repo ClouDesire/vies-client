c33ac47f0162c43a47ead84019c79a4086e8c91a
Giovanni Toraldo <g.toraldo@liberologico.com>
2015-06-29 15:14:19 +0200

    Minor fix: code reformatting

--- src/main/java/dk/es/br/vies/ViesVatService.java
+++ src/main/java/dk/es/br/vies/ViesVatService.java
@@ -2,50 +2,50 @@ package dk.es.br.vies;
 
 import eu.europa.ec.taxud.vies.services.checkvat.CheckVatPortType;
 import eu.europa.ec.taxud.vies.services.checkvat.CheckVatService;
-import java.util.ResourceBundle;
+
 import javax.xml.datatype.XMLGregorianCalendar;
 import javax.xml.soap.SOAPFault;
 import javax.xml.ws.Holder;
 import javax.xml.ws.soap.SOAPFaultException;
+import java.util.ResourceBundle;
 
-/**
- * @author      osa
- * @since       10-05-2013
- */
-public class ViesVatService {
-    private final CheckVatService svc = new CheckVatService();
+public class ViesVatService
+{
+	private final CheckVatService svc = new CheckVatService();
 
-    public ViesVatRegistration lookup(String country, String vatNumber) throws ViesVatServiceException {
-        CheckVatPortType cv = svc.getCheckVatPort();
+	public ViesVatRegistration lookup ( String country, String vatNumber ) throws ViesVatServiceException
+	{
+		CheckVatPortType cv = svc.getCheckVatPort();
 
-        Holder<String> country_ = new Holder(country);
-        Holder<String> vatNumber_ = new Holder(vatNumber);
-        Holder<XMLGregorianCalendar> date_ = new Holder<XMLGregorianCalendar>();
-        Holder<Boolean> valid_ = new Holder<Boolean>();
-        Holder<String> name_ = new Holder<String>();
-        Holder<String> address_ = new Holder<String>();
+		Holder<String> country_ = new Holder( country );
+		Holder<String> vatNumber_ = new Holder( vatNumber );
+		Holder<XMLGregorianCalendar> date_ = new Holder<XMLGregorianCalendar>();
+		Holder<Boolean> valid_ = new Holder<Boolean>();
+		Holder<String> name_ = new Holder<String>();
+		Holder<String> address_ = new Holder<String>();
 
-        try {
-            cv.checkVat(country_, vatNumber_, date_, valid_, name_, address_);
-        }
-        catch (SOAPFaultException ex) {
-            SOAPFault fault = ex.getFault();
-            String faultKey = fault.getFaultString();
-            String faultMessage = ResourceBundle.getBundle(ViesVatService.class.getName()).getString("vies.fault." + faultKey);
-            throw new ViesVatServiceException(faultKey, country + "-" + vatNumber + ": " + faultMessage);
-        }
+		try
+		{
+			cv.checkVat( country_, vatNumber_, date_, valid_, name_, address_ );
+		} catch ( SOAPFaultException ex )
+		{
+			SOAPFault fault = ex.getFault();
+			String faultKey = fault.getFaultString();
+			String faultMessage = ResourceBundle.getBundle( ViesVatService.class.getName() )
+					.getString( "vies.fault." + faultKey );
+			throw new ViesVatServiceException( faultKey, country + "-" + vatNumber + ": " + faultMessage );
+		}
 
-        if (!valid_.value)
-            return null;
+		if ( !valid_.value ) return null;
 
-        ViesVatRegistration res = new ViesVatRegistration();
+		ViesVatRegistration res = new ViesVatRegistration();
 
-        res.setCountry(country_.value);
-        res.setVatNumber(vatNumber_.value);
-        res.setRequestDate(date_.value.toGregorianCalendar().getTime());
-        res.setName(name_.value);
-        res.setAddress(address_.value);
+		res.setCountry( country_.value );
+		res.setVatNumber( vatNumber_.value );
+		res.setRequestDate( date_.value.toGregorianCalendar().getTime() );
+		res.setName( name_.value );
+		res.setAddress( address_.value );
 
-        return res;
-    }
+		return res;
+	}
 }
--- src/main/java/dk/es/br/vies/ViesVatServiceException.java
+++ src/main/java/dk/es/br/vies/ViesVatServiceException.java
@@ -1,18 +1,17 @@
 package dk.es.br.vies;
 
-/**
- * @author      osa
- * @since       21-06-2013
- */
-public class ViesVatServiceException extends Exception {
-    private String errorKey;
+public class ViesVatServiceException extends Exception
+{
+	private String errorKey;
 
-    public ViesVatServiceException(String key, String message) {
-        super(message);
-        this.errorKey = key;
-    }
+	public ViesVatServiceException ( String key, String message )
+	{
+		super( message );
+		this.errorKey = key;
+	}
 
-    public String getErrorKey() {
-        return errorKey;
-    }
+	public String getErrorKey ()
+	{
+		return errorKey;
+	}
 }
