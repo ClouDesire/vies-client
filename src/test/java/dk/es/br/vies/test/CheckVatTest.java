b267e988927a5602e95b78ab44ee83b1e7f8bca6
Giovanni Toraldo <g.toraldo@liberologico.com>
2015-06-29 15:12:34 +0200

    Removed main method and added VATs into main test.

--- src/test/java/dk/es/br/vies/test/CheckVatTest.java
+++ src/test/java/dk/es/br/vies/test/CheckVatTest.java
@@ -25,11 +25,32 @@ public class CheckVatTest
 		try
 		{
 			ViesVatRegistration response = service.lookup( "IT", "02103430506" );
-
 			assertEquals( "IT", response.getCountry() );
 			assertEquals( "02103430506", response.getVatNumber() );
 			assertEquals( "CLOUDESIRE.COM SRL", response.getName() );
 			assertEquals( "VIA ADRIANO GOZZINI 15/17 \n" + "56121 PISA PI\n", response.getAddress() );
+		}
+		catch ( ViesVatServiceException e )
+		{
+			fail( "Lookup method returned " + e.getMessage() );
+		}
+
+		try
+		{
+			ViesVatRegistration response = service.lookup( "DK", "26033489" );
+			assertEquals( "DK", response.getCountry() );
+			assertEquals( "26033489", response.getVatNumber() );
+		}
+		catch ( ViesVatServiceException e )
+		{
+			fail( "Lookup method returned " + e.getMessage() );
+		}
+
+		try
+		{
+			ViesVatRegistration response = service.lookup( "EL", "054934613" );
+			assertEquals( "EL", response.getCountry() );
+			assertEquals( "054934613", response.getVatNumber() );
 		} catch ( ViesVatServiceException e )
 		{
 			fail( "Lookup method returned " + e.getMessage() );
